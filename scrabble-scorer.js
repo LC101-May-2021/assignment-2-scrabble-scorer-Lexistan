fd66df734f00248b6a66527468fab45240d63a5c
Kris Rehbein <84356496+Lexistan@users.noreply.github.com>
2021-06-16 20:13:31 +0000

    added scoring algorithms, created their objects and keys

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -32,19 +32,58 @@ function oldScrabbleScorer(word) {
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
-function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+function initialPrompt(data) {
+  data =  input.question("Let's play some scrabble! Enter a word:");
+  data = oldScrabbleScorer(data);
+  console.log(data)
+
 };
 
-let simpleScore;
+let simpleScore = function simpleScore(word) {
+  word = word.toUpperCase();
+  sscore = word.length
+  score = Number(sscore)
+  console.log(sscore)
+  return sscore
 
-let vowelBonusScore;
+let vowelBonusScore = function vowelBonusScore(word) {
+  word = word.toUpperCase();
+  const vowelArray = ["A", "E", "I", "O", "U"] //what's worth 3 points
+  let vbsArray = []; //the array for the word
+  let vbScore = 0 //the score we'll be updating
+  vbsArray = word.split("") //breaks the word into pieces
+  for (i = 0; i < word.length; i++) //standard loop
+    if (vowelArray.includes(word[i])) { // if our character is in the vowel array
+    vbScore = vbScore + 3 // its worth 3
+     } else { 
+       vbScore = vbScore + 1 
+     }
+       return vbScore //give back the updated score
 
 let scrabbleScore;
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [  //holds the keys (info) to all the scoring algorithms
 
-function scorerPrompt() {}
+simpleAlgorithm = {
+  simpleAlgorithmName: simpleAlgorithm,
+  simpleAlgorithmDescription: "Each letter is worth 1 point.",
+  simpleAlgorithmScorerFunction: simpleScore
+}
+vowelBonusAlgorithm = {
+  vowelBonusAlgorithmName: vowelBonusAlgorithm,
+  vowelBonusAlgorithmDescription: "Vowels are 3 pts, consonants are 1 pt.	",
+  vowelBonusAlgorithmFunction: vowelBonusScore
+scrabbleAlgorithm = {
+  scrabbleAlgorithmName: scrabbleAlgorithm,
+  scrabbleAlgorithmDescription: "The traditional scoring algorithm.	",
+  scrabbleAlgorithmFunction: oldScrabbleScorer
+
+}
+]
+function scorerPrompt() {
+  input.question("Select which scoring method to use: ")
+  
+}
 
 function transform() {};
 
